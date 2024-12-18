9822c4eb9a47a8e17ad44580d17dfca25e2721cb
apix <hewlettpin777@gmail.com>
2024-12-18 09:30:50 +0000

    Temp

---
+++ src/product-service/data.py
@@ -0,0 +1,72 @@
+INITIAL_PRODUCTS = [
+    {
+        "id": 1,
+        "name": "Contoso Catnip's Friend",
+        "price": 9.99,
+        "description": "Watch your feline friend embark on a fishing adventure with Contoso Catnip's Friend toy. Packed with irresistible catnip and dangling fish lure.",
+        "image": "/catnip.jpg"
+    },
+    {
+        "id": 2,
+        "name": "Salty Sailor's Squeaky Squid",
+        "price": 6.99,
+        "description": "Let your dog set sail with the Salty Sailor's Squeaky Squid. This interactive toy provides hours of fun, featuring multiple squeakers and crinkle tentacles.",
+        "image": "/squid.jpg"
+    },
+    {
+        "id": 3,
+        "name": "Mermaid's Mice Trio",
+        "price": 12.99,
+        "description": "Entertain your kitty with the Mermaid's Mice Trio. These adorable plush mice are dressed as mermaids and filled with catnip to captivate their curiosity.",
+        "image": "/mermaid.jpg"
+    },
+    {
+        "id": 4,
+        "name": "Ocean Explorer's Puzzle Ball",
+        "price": 11.99,
+        "description": "Challenge your pet's problem-solving skills with the Ocean Explorer's Puzzle Ball. This interactive toy features hidden compartments and treats, providing mental stimulation and entertainment.",
+        "image": "/ocean.jpg"
+    },
+    {
+        "id": 5,
+        "name": "Pirate Parrot Teaser Wand",
+        "price": 8.99,
+        "description": "Engage your cat in a playful pursuit with the Pirate Parrot Teaser Wand. The colorful feathers and jingling bells mimic the mischievous charm of a pirate's parrot.",
+        "image": "/pirate.jpg"
+    },
+    {
+        "id": 6,
+        "name": "Seafarer's Tug Rope",
+        "price": 14.99,
+        "description": "Tug-of-war meets nautical adventure with the Seafarer's Tug Rope. Made from marine-grade rope, it's perfect for interactive play and promoting dental health in dogs.",
+        "image": "/tug.jpg"
+    },
+    {
+        "id": 7,
+        "name": "Seashell Snuggle Bed",
+        "price": 19.99,
+        "description": "Give your furry friend a cozy spot to curl up with the Seashell Snuggle Bed. Shaped like a seashell, this plush bed provides comfort and relaxation for cats and small dogs.",
+        "image": "/bed.jpg"
+    },
+    {
+        "id": 8,
+        "name": "Nautical Knot Ball",
+        "price": 7.99,
+        "description": "Unleash your dog's inner sailor with the Nautical Knot Ball. Made from sturdy ropes, it's perfect for fetching, tugging, and satisfying their chewing needs.",
+        "image": "/knot.jpg"
+    },
+    {
+        "id": 9,
+        "name": "Contoso Claw's Crabby Cat Toy",
+        "price": 3.99,
+        "description": "Watch your cat go crazy for Contoso Claw's Crabby Cat Toy. This crinkly and catnip-filled toy will awaken their hunting instincts and provide endless entertainment.",
+        "image": "/crabby.jpg"
+    },
+    {
+        "id": 10,
+        "name": "Ahoy Doggy Life Jacket",
+        "price": 5.99,
+        "description": "Ensure your furry friend stays safe during water adventures with the Ahoy Doggy Life Jacket. Designed for dogs, this flotation device offers buoyancy and visibility in style.",
+        "image": "/lifejacket.jpg"
+    }
+]
